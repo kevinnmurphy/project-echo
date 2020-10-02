117d656c58c0a30e8ac78842ec106bfadb23aa80
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-10-02 00:21:10 -0400

    song display

--- src/features/songs/SongCard.js
+++ src/features/songs/SongCard.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-export default function SongCard() {
-  return <div>Song: title, artist,</div>;
+import Card from 'react-bootstrap/card';
+
+// function SongCard({ id, name, artist, spotify_id }) {
+//   const handleCardClick = () => {};
+
+function SongCard({ id, name, artist, position, time }) {
+  return (
+    <Card
+      className='song-card button muted-button'
+      style={{ width: 'auto' }}
+      // onClick={handleCardClick}
+    >
+      {/* <Card.Img variant='top' src={pic_url} className='song-pic' alt={id} /> */}
+      <Card.Text>{position}</Card.Text>
+      <Card.Title>{name}</Card.Title>
+      <Card.Text>{artist}</Card.Text>
+      <Card.Text>{time}</Card.Text>
+
+      {/* <Card.Img variant='top' src={pic_url} className='song-pic' alt={id} /> */}
+    </Card>
+  );
 }
+
+export default SongCard;
