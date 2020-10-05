02abf65679e93e50d802a9e913cf1b329ca7451b
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-10-04 23:07:19 -0400

    edit author

--- src/features/playlists/PlaylistAuthor.js
+++ src/features/playlists/PlaylistAuthor.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectUserById, selectUsersByPlaylist } from '../users/usersSlice';
 
-export const PlaylistAuthor = ({ userId, playlistId }) => {
+const PlaylistAuthor = ({ userId, playlistId }) => {
   const author = useSelector(
     (state) => selectUserById(state, userId[0].id).name
   );
-  let authorList;
 
   // const author = useSelector((state) =>
   //   selectUsersByPlaylist(state, playlistId)
@@ -14,3 +13,5 @@ export const PlaylistAuthor = ({ userId, playlistId }) => {
   // debugger;
   return <p>by {author ? author : 'Unknown'}</p>;
 };
+
+export default PlaylistAuthor;
