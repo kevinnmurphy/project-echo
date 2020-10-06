c0399b5054ac6fded997d441c550f83862efd60d
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-10-06 09:09:08 -0400

    login hash

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Loading from './Loading';
 import Iframe from '../features/iframe/iframe';
 
-function Home() {
+import { Button } from 'react-bootstrap';
+
+import chunk from 'lodash/chunk';
+import { getParamValues } from '../features/spotify/functions';
+import _ from 'lodash';
+
+function Home(props) {
+  const {
+    REACT_APP_CLIENT_ID,
+    REACT_APP_AUTHORIZE_URL,
+    REACT_APP_REDIRECT_URL,
+  } = process.env;
+
+  const handleLogin = () => {
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+  };
+
   return (
     <div>
-      {/* <script src='https://sdk.scdn.co/spotify-player.js'></script> */}
       <h4>Home</h4>
+      <Button variant='info' type='submit' onClick={handleLogin}>
+        Login to spotify
+      </Button>
       <Loading />
       <Iframe />
     </div>
---
+++ src/app/RedirectPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import _ from 'lodash';
+import { getParamValues } from '../features/spotify/functions';
+
+export default class RedirectPage extends React.Component {
+  debugger;
+  componentDidMount() {
+    const { setExpiryTime, history, location } = this.props;
+    try {
+      if (_.isEmpty(location.hash)) {
+        return history.push('/');
+      }
+      const access_token = getParamValues(location.hash);
+      const expiryTime = new Date().getTime() + access_token.expires_in * 1000;
+      localStorage.setItem('params', JSON.stringify(access_token));
+      localStorage.setItem('expiry_time', expiryTime);
+      history.push('/');
+    } catch (error) {
+      history.push('/');
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
