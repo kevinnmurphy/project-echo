50b66a6cad80a0df61a4758e714d3d161d60a21c
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-12-21 22:26:05 -0500

    change module to card

--- src/features/songs/SongCard.js
+++ src/features/songs/SongCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Card from 'react-bootstrap/card';
+import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
